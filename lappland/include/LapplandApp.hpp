401ce3940e1819ae0d808d3b72c91ed698192927
Fusion86 <fusion86@cerbus.nl>
2020-04-06 13:10:32 +0200

    Wip

--- lappland/include/LapplandApp.hpp
+++ lappland/include/LapplandApp.hpp
@@ -1,10 +1,17 @@
 #pragma once
 
+#include <memory>
+
+#include <HorizonSaveData.hpp>
+
 #include "wx.hpp"
 
 class LapplandApp : public wxApp {
   public:
     virtual bool OnInit();
+
+  private:
+    std::unique_ptr<HorizonSaveData> savedata;
 };
 
 class MyFrame : public wxFrame {
@@ -12,14 +19,20 @@ class MyFrame : public wxFrame {
     MyFrame(const wxString& title, const wxPoint& pos, const wxSize& size);
 
   private:
+    void OnOpenFile(wxCommandEvent& event);
     void OnExit(wxCommandEvent& event);
     void OnAbout(wxCommandEvent& event);
     wxDECLARE_EVENT_TABLE();
 };
 
+enum {
+    ID_OpenFile = 1,
+};
+
 // clang-format off
 wxBEGIN_EVENT_TABLE(MyFrame, wxFrame)
-    EVT_MENU(wxID_EXIT,  MyFrame::OnExit)
+    EVT_MENU(ID_OpenFile, MyFrame::OnOpenFile)
+    EVT_MENU(wxID_EXIT, MyFrame::OnExit)
     EVT_MENU(wxID_ABOUT, MyFrame::OnAbout)
 wxEND_EVENT_TABLE();
 // clang-format on
--- lappland/src/LapplandApp.cpp
+++ lappland/src/LapplandApp.cpp
@@ -1,10 +1,14 @@
 #include "LapplandApp.hpp"
 
-#include <wx/settings.h>
+#include <filesystem>
+
+namespace fs = std::filesystem;
 
 wxIMPLEMENT_APP(LapplandApp);
 
 bool LapplandApp::OnInit() {
+    savedata = std::make_unique<HorizonSaveData>();
+
     wxFrame* frame = new MyFrame("Hello World", wxPoint(500, 500), wxSize(500, 500));
     frame->Show(true);
     return true;
@@ -12,8 +16,32 @@ bool LapplandApp::OnInit() {
 
 MyFrame::MyFrame(const wxString& title, const wxPoint& pos, const wxSize& size)
     : wxFrame(NULL, wxID_ANY, title, pos, size) {
+    wxMenu* menuFile = new wxMenu();
+    menuFile->Append(ID_OpenFile, "&Open\tCtrl-O", "Open savedata");
+    menuFile->AppendSeparator();
+    menuFile->Append(wxID_EXIT);
+
+    wxMenu* menuHelp = new wxMenu();
+    menuHelp->Append(wxID_ABOUT);
+
+    wxMenuBar* menuBar = new wxMenuBar();
+    menuBar->Append(menuFile, "&File");
+    menuBar->Append(menuHelp, "&Help");
+
+    SetMenuBar(menuBar);
     CreateStatusBar();
     SetStatusText("So far so good.");
+}
+
+void MyFrame::OnOpenFile(wxCommandEvent& event) {
+    wxDirDialog* dialog = new wxDirDialog(this, "Select directory containing savedata", "",
+                                          wxDD_DEFAULT_STYLE | wxDD_DIR_MUST_EXIST);
+
+    if (dialog->ShowModal() == wxID_OK) {
+        fs::path path(dialog->GetPath().ToStdString());
+    }
+
+    dialog->Destroy();
 }
 
 void MyFrame::OnExit(wxCommandEvent& event) {
@@ -21,5 +49,6 @@ void MyFrame::OnExit(wxCommandEvent& event) {
 }
 
 void MyFrame::OnAbout(wxCommandEvent& event) {
-    wxMessageBox("This is a wxWidgets' Hello world sample", "About Hello World", wxOK | wxICON_INFORMATION);
+    wxMessageBox("This is a wxWidgets' Hello world sample", "About Hello World",
+                 wxOK | wxICON_INFORMATION);
 }
